eca72b4f57e2c75cfac7d147d2bb9c5e77722e8f
tayleredelen <taylerledelen@gmail.com>
2024-07-15 11:22:45 -0500

    if else chapter work

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon.");
+            }
+            else 
+            {
+                Console.WriteLine("It's evening.");
+            }
+
+        }
+    }
+}
